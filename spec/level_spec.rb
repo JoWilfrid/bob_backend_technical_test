a9451ecfc0ba08e94befbdfc3d16ad12d0a06796
Johann Wilfrid-Calixte <johann@skello.io>
2017-12-19 23:35:06 +0100

    :white_check_mark: Add tests for level 1

--- spec/level_spec.rb
+++ spec/level_spec.rb
@@ -3,6 +3,9 @@
 require 'level'
 
 RSpec.describe Level do
+  let(:data_file) { File.join(APPLICATION_ROOT, 'data/output.json') }
+  let(:expectation) { JSON.parse(File.read(data_file)) }
+
   describe '#initialize' do
     it 'should init without params' do
       expect(Level.new).not_to be_nil
@@ -18,6 +21,16 @@ RSpec.describe Level do
   describe '#result' do
     it 'should have a method result' do
       expect { subject.result }.not_to raise_error NoMethodError
+    end
+
+    it 'should return a non-nil result' do
+      expect(subject.result).not_to be_nil
+    end
+
+    it 'should meet the expectations' do
+      subject.run
+
+      expect(subject.result).to eq expectation
     end
   end
 end
