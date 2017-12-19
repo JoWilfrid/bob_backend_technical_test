62008d9c57b32cdede68d1225f05c32fed8fde08
Johann Wilfrid-Calixte <johann@skello.io>
2017-12-20 00:08:03 +0100

    :sparkles: Complete level 2

--- app/level.rb
+++ app/level.rb
@@ -9,6 +9,12 @@ class Level
 
   # Constants
   DATA_FILE_PATH = 'data/data.json'
+  DECREASE = [] # rubocop:disable Style/MutableConstant
+  DECREASE.fill(0, 0..1)
+  DECREASE.fill(0.1, 2..4)
+  DECREASE.fill(0.3, 5..10)
+  DECREASE.fill(0.5, 11..3650)
+  DECREASE.freeze
 
   def result
     { rentals: @rentals }
@@ -45,6 +51,13 @@ class Level
   end
 
   def rental_price(car, days, distance)
-    car['price_per_day'] * days + car['price_per_km'] * distance
+    return (car['price_per_day'] * days + car['price_per_km'] * distance) if days <= 1
+
+    duration_price = (0..days).to_a.inject do |sum, day|
+      day_price = car['price_per_day'] - car['price_per_day'] * DECREASE[day]
+      sum + day_price
+    end
+
+    duration_price + car['price_per_km'] * distance
   end
 end
